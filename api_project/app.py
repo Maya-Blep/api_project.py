a7b567232f44ebbc2cacfafffcfdeda26d511cba
Maya-Blep <maya.hartley@csedge.org>
2023-02-11 16:05:19 -0500

    origin

--- api_project/app.py
+++ api_project/app.py
@@ -3,9 +3,21 @@ import requests
 app = Flask(__name__)
 
 @app.route("/")
-def get_movies():
-    response = requests.get("https://animechan.vercel.app/api/random")
+def index():
+    return render_template ("index.html")
 
+@app.route("/quote")
+def get_quotes():
+    response = requests.get("https://animechan.vercel.app/api/random")
     data = response.json()
+    #data = {'anime': 'Avatar: The Last Airbender', 'character': 'Toph Bei Fong', 'quote': "Somebody's a little light on his feet. What's your fighting name, the Fancy Dancer?"}
     print(data)
-    return render_template ("index.html", data)
+    return render_template ("quote.html", data = data)
+
+
+
+if __name__== '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+
+#<img src= "https://i.pinimg.com/originals/05/bc/84/05bc8496dfae86b4bcb185204591f4fc.jpg">
